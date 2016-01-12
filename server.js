204a78ce3c5d85a5bc19426d54262d943de28598
Guillermo Londono <grillonegro@gmail.com>
2016-01-12 14:23:30 +0000

    Convert html app to node app to be run on heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var compression = require('compression');
+
+var app = express();
+
+
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use('/',express.static(__dirname));
+
+//Start server
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port) + " " + process.env.NODE_ENV;
+});
